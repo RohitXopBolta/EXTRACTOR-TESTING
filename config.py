c39e3cca995a0ea0588bd7c0faf7c0c854545bdc
RohitXopBolta <rohitpapop766@gmail.com>
2025-07-31 09:37:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@ import os
 
 class Config(object):
     # Telegram Bot ka token
-    BOT_TOKEN = ""
+    BOT_TOKEN = "8384280924:AAHLTy-oV2YA1cJXobviZU8Tdkd1vM3HUHs"
     # Telegram API ki ID
     API_ID = "26548330"
     API_HASH = "244c3afd019c7b0cd1a8184cd2be2495"
     ADMIN_ID = [6890753169]
     # MongoDB database ka URL
-    DB_URL = ""
+    DB_URL = "mongodb+srv://hshssdf88:hshssdf88@cluster0.cujuzaz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     # Database ka naam
     DB_NAME = "MY_BOT_DB"
     # Text log channel ki ID
